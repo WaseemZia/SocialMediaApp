04a11dd66a1f3d8c34ccb2c21a04997027c37b4c
WaseemZia <waseemzia80@gmail.com>
2025-05-30 22:34:59 +0500

    Add project files.

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,33 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+
+        private readonly AppDbContext context;
+        public ActivitiesController(AppDbContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            
+            return await context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{Id}")]
+
+        public async Task<ActionResult<Activity>> GetActivitiesById(string Id)
+        {
+            var activities = await context.Activities.FindAsync(Id);
+            if (activities == null) { return NotFound(); }
+            return activities;
+        }
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await DbInitializer.SeedData(context);
+    
+}catch(Exception ex)
+{
+     var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An Exception occurred");
+}
+app.Run();
